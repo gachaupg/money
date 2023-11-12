3af9f37f73efd9203bfd9b0217a52c6085b20d19
gachaupg <gachaupg607@gmail.com>
2023-11-12 19:13:08 +0300

    first commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -42,65 +42,44 @@ const Content = () => {
   };
   return (
     <div>
-      <h2>YAFREEKA MONETIZATION MODEL.</h2>
+      <h2 style={{ textDecoration: "capitalize" }}>TERMS AND CODITIONS .</h2>
       <hr />
       <p>
-        At <a href="https://home.yafreeka.com/"> Yafreeka's</a>, we understand the value of originality and the hard work
-        that goes into creating unique and engaging content. As a part of our
-        commitment to supporting creators, we have developed a Monetization
-        Model designed to reward and empower those who contribute to the
-        diversity and creativity of our platform.
+        Welcome to <a href="https://home.yafreeka.com/"> Yafreeka's</a>, These
+        Terms and Conditions ("Terms") are a legally binding agreement between
+        you and yafreeka. By accessing or using the App, you agree to comply
+        with and be bound by these Terms. If you do not agree to these Terms,
+        please do not use the App.
       </p>
       <hr />
       <div className="first">
-        <h4 style={{ fontWeight: "700" }}>Eligibility Criteria</h4>
+        <h4 style={{ textTransform: "uppercase" }}>
+          USER REGISTRATION AND ACCOUNTS{" "}
+        </h4>
         <h6>
-          To be eligible for monetization on Yafreeka, content creators are
-          required to:
+          <GoDotFill /> . To use certain features of the App, you may be
+          required to create an account. You are responsible for maintaining the
+          confidentiality of your account credentials and for all activities
+          that occur under your account.
         </h6>
-       
-        {/* <div className="positioning">
-          {show1 ? (
-            <>
-              <p onClick={handleClick1}>Learn more</p>
-              <IoIosArrowUp
-                onClick={handleClick1}
-                className="icon"
-                size={24}
-                color="rgb(44, 44, 185)"
-              />
-            </>
-          ) : (
-            <>
-              <p onClick={handleClick1}>Learn more</p>
-              <IoIosArrowDown
-                onClick={handleClick1}
-                className="icon"
-                size={24}
-                color="rgb(44, 44, 185)"
-              />
-            </>
-          )}{" "}
-        </div>
-        {show1 && ( */}
-          <p style={{ textDecoration: "dotted" }}>
-            <GoDotFill /> Post 20 Original Videos: Creators must upload a
-            minimum of 20 original videos on the Yafreeka platform. These videos
-            should showcase the creator's unique voice, perspective, and
-            creative touch.
-          </p>
+        <p style={{ textDecoration: "dotted" }}>
+          <GoDotFill /> . You must be at least 13 years of age to use the App.
+          If you are under 13, you may only use the App with the consent of a
+          parent or guardian.
+        </p>
         {/* )} */}
 
         <hr />
       </div>
 
       <div className="first">
-        <h4 style={{ fontWeight: "700" }}>Monetization Application Process:</h4>
+        <h4 style={{ textTransform: "uppercase" }}>CONTENT GUIDELINES :</h4>
         <h6>
-          Upon reaching the milestone of 20 original videos, creators can apply
-          for monetization by following these simple steps:
+          Users may upload, share, and view various forms of content on the App,
+          including videos, comments, and other media. By using the App, you
+          agree to adhere to the following content guidelines: :
         </h6>
-        {/* <hr />
+        <hr />
         <div className="positioning">
           {show2 ? (
             <>
@@ -124,26 +103,45 @@ const Content = () => {
             </>
           )}{" "}
         </div>
-        {show2 && ( */}
+        {show2 && (
           <p style={{ textDecoration: "dotted" }}>
-            <GoDotFill /> Send an Email: Submit an application to{" "}
-            <a href="care@yafreeka.com">care@yafreeka.com</a> . In the email,
-            include your Yafreeka username, a brief description of your channel,
-            and any other relevant details you believe would support your
-            application.
+            <ul>
+              <li>
+                {" "}
+                You will not upload, share, or promote any content that is
+                illegal, infringes upon intellectual property rights, or
+                violates applicable laws and regulations.
+              </li>
+              <li>
+                You will not post content that is harmful, offensive,
+                defamatory, or discriminatory towards any individual or group
+                based on race, religion, gender, sexual orientation, disability,
+                or any other characteristic.
+              </li>
+              <li>
+                You will not engage in harassment, cyberbullying, or any other
+                harmful behaviour towards other users.{" "}
+              </li>
+              <li>
+                You will not post any content containing any sexual contents or
+                adult scenes.{" "}
+              </li>
+            </ul>
           </p>
-        {/* )} */}
+        )}
 
         <hr />
       </div>
 
       <div className="first">
-        <h4 style={{ fontWeight: "700" }}>Video Review Process:</h4>
+        <h4 style={{ textTransform: "uppercase" }}>INTELLECTUAL PROPERTY :</h4>
         <h6>
-        <a href="https://home.yafreeka.com/"> Yafreeka's</a> is committed to ensuring that monetization is awarded to
-          creators who contribute original and valuable content to the platform.
-          Our team will carefully review the submitted 20 videos to assess their
-          originality.{" "}
+          All content on the App, including but not limited to text, graphics,
+          videos, and logos, is the property of the Company or its licensors and
+          is protected by copyright and other intellectual property laws.
+          <br /> Users may not use, reproduce, distribute, or modify any content
+          on the App without the explicit permission of the owner, except as
+          expressly allowed under these Terms.
         </h6>{" "}
         {/* <hr />
         <div className="positioning">
@@ -170,24 +168,23 @@ const Content = () => {
           )}{" "}
         </div>
         {show3 && ( */}
-          <p style={{ textDecoration: "dotted" }}>
-            <GoDotFill /> Definition of Original Content: Original content is
-            considered to be entirely created by the user, reflecting their
-            unique style, perspective, and creative input. Yafreeka encourages
-            diversity and authenticity, and we celebrate content that stands out
-            from the crowd.
-          </p>
+        {/* <p style={{ textDecoration: "dotted" }}>
+          <GoDotFill /> Definition of Original Content: Original content is
+          considered to be entirely created by the user, reflecting their unique
+          style, perspective, and creative input. Yafreeka encourages diversity
+          and authenticity, and we celebrate content that stands out from the
+          crowd.
+        </p> */}
         {/* )} */}
         <hr />
       </div>
 
       <div className="first">
-        <h4 style={{ fontWeight: "700" }}>Monetization Approval:</h4>
+        <h4 style={{ textTransform: "uppercase" }}>PRIVACY :</h4>
         <h6>
-          If your videos pass the review process and are confirmed as original,
-          congratulations! You will be approved for monetization on Yafreeka.
-          This means you can start earning revenue from your content, turning
-          your passion into a potential income stream.{" "}
+          Your use of the App is subject to our Privacy Policy, which outlines
+          how we collect, use, and protect your personal information. By using
+          the App, you consent to the practices described in the Privacy Policy.{" "}
         </h6>
         {/* <div className="positioning">
           {show3 ? (
@@ -225,148 +222,56 @@ const Content = () => {
         <hr />
       </div>
       <div className="first">
-        <h3 style={{ fontWeight: "700" }}>
-          Setting and Editing Content Prices on Yafreeka:
-        </h3>
+        <h4 style={{ textTransform: "uppercase" }}>TERMINATION :</h4>
 
-        <h5>A Step-by-Step Guide</h5>
         <h6>
-          At <a href="https://home.yafreeka.com/"> Yafreeka's</a>, we believe in giving creators the flexibility to
-          determine the value of their content. Our platform allows you to set
-          and edit the prices for your content easily, empowering you to take
-          control of your earnings.
+          The Company reserves the right to terminate or suspend your account
+          and access to the App, at its sole discretion, for any reason,
+          including but not limited to violations of these Terms.
           <br />
-          <div style={{ marginTop: ".5rem" }}></div>
-          <strong style={{ marginTop: "1rem" }}>
-            {" "}
-            Follow this step-by-step guide to customize the pricing of your
-            content.
-          </strong>
-          <hr />
         </h6>
-        <div className="positioning">
-          {show4 ? (
-            <>
-              <p onClick={handleClick4}>Learn more</p>
-              <IoIosArrowUp
-                onClick={handleClick4}
-                className="icon"
-                size={24}
-                color="rgb(44, 44, 185)"
-              />
-            </>
-          ) : (
-            <>
-              <p onClick={handleClick4}>Learn more</p>
-              <IoIosArrowDown
-                onClick={handleClick4}
-                className="icon"
-                size={24}
-                color="rgb(44, 44, 185)"
-              />
-            </>
-          )}{" "}
-        </div>
-        {show4 && (
-          <>
-            <div>
-              <h4 style={{ fontWeight: "520" }}>
-                Accessing Your Account Settings:
-              </h4>
-              <ul>
-                <li>Log in to your Yafreeka account</li>
-                <li>Click the account tab</li>
-                <li>Select "Settings" from the top right of the page</li>
-              </ul>
-            </div>
-            <hr />
-            <div>
-              <h4 style={{ fontWeight: "520" }}>Click the account tab:</h4>
-              <ul>
-                <li>Within the account menu, look for an option “premium”</li>
-                <li>
-                  This will take you to the section where you can manage the
-                  pricing of your content.
-                </li>
-              </ul>
-            </div>
-            <hr />
-            <div>
-              <h4 style={{ fontWeight: "520" }}>Setting the Initial Prices:</h4>
-              <ul>
-                <li>
-                  In the Content Pricing section, you'll see a dialogue box.
-                </li>
-                <li>Enter the desired price for your content.</li>
-                <li>Save your changes.</li>
-              </ul>
-            </div>
-            <hr />
-            <div>
-              <h4 style={{ fontWeight: "520" }}>Editing Content Prices:</h4>
-              <ul>
-                <li>
-                  To edit the prices of your content, navigate back to the
-                  premium section in your settings.
-                </li>
-                <li>
-                  A tab will open where you can change the price from what you
-                  initially set.
-                </li>
-                <li>Confirm the changes.</li>
-              </ul>
-            </div>
-            <hr />
-            <div>
-              <h4 style={{ fontWeight: "520" }}>Additional Tips:</h4>
-              <ul>
-                <li>
-                  Keep in mind that pricing should be fair and reflective of the
-                  value you believe your content providers.
-                </li>
-                <li>
-                  Regularly assess and update your pricing strategy based on
-                  audience feedback, market trends, or changes in your content's
-                  quality.
-                </li>
-              </ul>
-            </div>
-            <hr />
-            <div>
-              <h4 style={{ fontWeight: "520" }}>
-                Contact Support for Assistance:
-              </h4>
-              <ul>
-                <li>
-                  - If you encounter any issues or have questions about setting
-                  or editing content prices, feel free to reach out to our
-                  support team at{" "}
-                  <a href="care@yafreeka.com">care@yafreeka.com</a>. We're here
-                  to assist you.
-                </li>
-                <li>
-                  Empower yourself as a Yafreeka creator by customizing the
-                  prices of your content to match your unique value proposition.
-                </li>
-              </ul>
-            </div>
-          </>
-        )}
-
-        <hr />
       </div>
+      <hr />
+      <div className="first">
+        <h4 style={{ textTransform: "uppercase" }}>
+          DISCLAIMERS AND LIMITATIONS OF LIABILITY :
+        </h4>
+
+        <h6>
+          The App is provided "as is," and the Company makes no warranties or
+          representations regarding its availability, functionality, or
+          accuracy.
+          <br /> The Company shall not be liable for any direct, indirect,
+          incidental, special, or consequential damages arising out of or in
+          connection with your use of the App .
+          <br />
+        </h6>
+      </div>
+      <hr />
       <div>
-        <h4 style={{ fontWeight: "520" }}>Conclusion:</h4>
+        <h4 style={{ textTransform: "uppercase" }}>CHANGES TO TERMS :</h4>
         <p>
-          <a href="https://home.yafreeka.com/"> Yafreeka's</a> Monetization
-          Model is designed to be fair, transparent, and supportive of creators
-          who contribute to the vibrancy of our platform. We believe that by
-          rewarding originality, we can foster a community that values
-          creativity and innovation. <br />
-          For any inquiries or to initiate the monetization application process,
-          please contact us at <a href="care@yafreeka.com">care@yafreeka.com</a> . We look forward to seeing the
-          incredible content you bring to Yafreeka, and we're excited to support
-          you on your journey as a creator.
+          The Company may update these Terms at any time. We will notify users
+          of any material changes. Your continued use of the App after such
+          changes constitutes acceptance of the new Terms.
+        </p>
+      </div>
+      <hr />
+      <div>
+        <h4 style={{ textTransform: "uppercase" }}>GOVERNING LAW :</h4>
+        <p>
+          . These Terms are governed by and construed in accordance with the
+          laws of kenya, without regard to its conflict of law principles.
+        </p>
+      </div>
+      <hr />
+
+      <div>
+        <h4 style={{ textTransform: "uppercase" }}>CONTACT INFORMATION :</h4>
+        <p>
+          If you have any questions or concerns regarding these Terms, please
+          contact us at [Contact Email]. By using the App, you acknowledge that
+          you have read, understood, and agreed to these Terms and Conditions.
         </p>
       </div>
       <hr />
